46becc8ea8e79d52ddd6f977c5f306814f82af15
Wendel Marins <wendeldemarins@hotmail.com>
2023-11-21 00:54:48 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/appJogos/AppJogosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJogosApplication.class, args);
+	}
+
+}
